d0dd2774c93230f49fa25e32f187b8ead94f8385
chenxiaolong <403228075@qq.com>
2017-09-27 16:09:29 +0800

    session中追加数据--购物车页面，修复

--- resources/views/study/session/cat.blade.php
+++ resources/views/study/session/cat.blade.php
@@ -110,6 +110,20 @@
             </div>
         </div>
     </div>
+
+    <pre>
+        return [
+
+            [ 'id'=>1, 'name'=>'联想电脑', 'price'=>'3999', 'img'=>'/assets/1.jpg','created_at'=>'2017-09-26 13:20:56' ],
+            [ 'id'=>2, 'name'=>'苹果电脑', 'price'=>'6999', 'img'=>'/assets/2.jpg','created_at'=>'2017-09-26 13:20:20' ],
+            [ 'id'=>3, 'name'=>'索尼电脑', 'price'=>'4999',  'img'=>'/assets/3.jpg','created_at'=>'2017-09-26 13:20:36' ],
+            [ 'id'=>4, 'name'=>'戴尔电脑', 'price'=>'5999',  'img'=>'/assets/4.jpg','created_at'=>'2017-09-26 13:20:08' ],
+
+        ];
+    </pre>
+
+    {{ dd($data) }}
+
 </div>
 
 </body>
